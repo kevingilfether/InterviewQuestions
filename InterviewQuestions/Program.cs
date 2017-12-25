e5fd660961e3dcc4da59ab33b308306cd7a1a7de
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-25 08:41:49 -0500

    Set up intro

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -6,7 +6,18 @@ namespace InterviewQuestions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //12-25
+            //InterviewCake - Stock Prices
+            //https://www.interviewcake.com/question/javascript/stock-price
+
+
+
+
+        }
+
+        public static int GetMaxProfit(int[] prices)
+        {
+            
         }
     }
 }
