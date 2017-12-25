72932c16790a2a8d248744edca9a2e17fa651cc8
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-25 08:49:07 -0500

    Initialized useful variables

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -17,7 +17,20 @@ namespace InterviewQuestions
 
         public static int GetMaxProfit(int[] prices)
         {
-            
+            //return this
+            int maxProfit = 0;
+            //This is the minimum before the maximum
+            int minLocal = 0;
+            int minIndex = 0;
+            //Max and index
+            int max = 0;
+            int maxIndex = 0;
+            for (int i = 0; i< prices.Length; i++)
+            {
+                if (prices[i] > max)
+                    max = prices[i];
+                if price
+            }
         }
     }
 }
