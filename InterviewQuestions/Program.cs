88f03c51af4de109332feb5d3aee35612c8ff239
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-25 09:02:55 -0500

    Finished broken algorithm for Get max price

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -10,12 +10,15 @@ namespace InterviewQuestions
             //InterviewCake - Stock Prices
             //https://www.interviewcake.com/question/javascript/stock-price
 
+            int[] prices = { 15, 20, 21, 35, 5, 34 };
+
+            Console.WriteLine(GetMaxProfitBroken(prices));
 
 
 
         }
 
-        public static int GetMaxProfit(int[] prices)
+        public static int GetMaxProfitBroken(int[] prices)
         {
             //return this
             int maxProfit = 0;
@@ -35,11 +38,14 @@ namespace InterviewQuestions
 
             }
             for (int i = 0; i<prices.Length; i++ )
-            { 
+            {
                 if (i == 0)
                     minLocal = prices[i];
                 else if (prices[i] < minLocal && i < maxIndex)
+                    minLocal = prices[i];
             }
+            maxProfit = max - minLocal;
+            return maxProfit;
         }
     }
 }
