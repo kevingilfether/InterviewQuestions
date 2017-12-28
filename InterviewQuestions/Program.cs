e9109951a7fdaf0078206970bd4632042db5b658
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-28 15:11:33 -0500

    Added score counts array, starting first for loop

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -14,10 +14,14 @@ namespace InterviewQuestions
 
         }
 
-        public static int[] SortScores (int[] unsortedScores, int HighestPossibleScore)
+        public static int[] SortScores(int[] unsortedScores, int HighestPossibleScore)
+        {
+            int[] sortedScores[] = new int[unsortedScores.Length];
+            int[] scoreCounts[] = new int[HighestPossibleScore - 1];
 
-        
-
-    
+            for (int score =0; score<unsortedScores.Length; int++)
+            { }
+        }
+            
     }
 }
