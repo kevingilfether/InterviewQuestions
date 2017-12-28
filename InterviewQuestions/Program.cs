b1e9e1e36b7f3371fc01e8cf26800311254fe97e
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-28 15:38:30 -0500

    Added copy to main

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -6,12 +6,15 @@ namespace InterviewQuestions
     {
         static void Main(string[] args)
         {
-            //11-25 Sort Scores on InterviewCake
+            //12-25 Sort Scores on InterviewCake
 
             int[] unsortedScores = new[] { 37, 89, 41, 65, 91, 53 };
             const int HighestPossibleScore = 100;
 
+            int[] sortedScores = SortScores(unsortedScores, HighestPossibleScore);
 
+            foreach (int score in sortedScores)
+                Console.WriteLine(score);
         }
 
         public static int[] SortScores(int[] unsortedScores, int HighestPossibleScore)
