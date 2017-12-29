e1494d0e08a0fccc0abcf533589ea9386575a90f
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-29 07:25:54 -0500

    Added Rectangle Class for Rectangle Intersection problem

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -6,41 +6,10 @@ namespace InterviewQuestions
     {
         static void Main(string[] args)
         {
-            //12-25 Sort Scores on InterviewCake
+            //12-29 RectangleIntersect on InterviewCake
 
-            int[] unsortedScores = new[] { 37, 89, 41, 65, 91, 53 };
-            const int HighestPossibleScore = 100;
-
-            int[] sortedScores = SortScores(unsortedScores, HighestPossibleScore);
-
-            foreach (int score in sortedScores)
-                Console.WriteLine(score);
         }
 
-        public static int[] SortScores(int[] unsortedScores, int HighestPossibleScore)
-        {
-            int[] sortedScores = new int[unsortedScores.Length];
-            int[] scoreCounts = new int[HighestPossibleScore + 1];
-
-            for (int score = 0; score < unsortedScores.Length; score++)
-            {
-                scoreCounts[unsortedScores[score] + 1]++;
-            }
-
-            int indexForSortedArray = 0;
-
-            for (int score = scoreCounts.Length-1; score >=0; score--)
-            {
-                int count = scoreCounts[score];
-
-                for (int occurence = 0; occurence < count; occurence++)
-                {
-                    sortedScores[indexForSortedArray] = score;
-                    indexForSortedArray++;
-                }
-            }
-            return sortedScores;
-        }
 
     }
 }
---
+++ InterviewQuestions/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestions
+{
+    class Rectangle
+    {
+        //XY coordinates of bottom corner
+        public int LeftX { get; set; }
+        public int BottomY { get; set; }
+
+        //Dimensions of rectangle
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(int leftX, int bottomY, int width, int height)
+        {
+            this.LeftX = leftX;
+            this.BottomY = bottomY;
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+
+    }
+}
