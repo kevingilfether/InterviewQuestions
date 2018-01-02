cb43700460636745358ccfb6a981186017fec27e
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-02 12:34:46 -0500

    Adding list searcher dictionary to find duplicates

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -7,11 +7,22 @@ namespace InterviewQuestions
     {
         static void Main(string[] args)
         {
-            int[] myNums = new int[] { 0, 2, 4, 6 };
+            int[] myNums = new int[] { 0, 2, 4, 6, 0 };
             LinkedList<int> myLinkedList = new LinkedList<int>();
 
             foreach (int num in myNums)
                 myLinkedList.AddLast(num);
+
+            // Writing Code to remove duplicates from LinkedList
+            Dictionary<int, int> listSearcher = new Dictionary<int, int>();
+
+            foreach (var num in myLinkedList)
+            {
+                if (listSearcher.ContainsKey(num))
+                    listSearcher[num]++;
+                else
+                    listSearcher.Add(num, 1);
+            }
         }
 
 
