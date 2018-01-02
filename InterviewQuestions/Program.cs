7300514a5685a755ce290498d38bd08e79669484
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-02 12:02:47 -0500

    Added LinkedList

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterviewQuestions
 {
@@ -6,7 +7,7 @@ namespace InterviewQuestions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList<int> myLinkedList = new LinkedList<int>();
         }
     }
 }
